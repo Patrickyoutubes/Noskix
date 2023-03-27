11cb32a149aed059a3bd19515e21f2d649548f7c
Patrickyoutubes <117463029+Patrickyoutubes@users.noreply.github.com>
2023-03-27 22:47:04 +0700

    Update world.lua

--- world.lua
+++ world.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/Noskix/main/world3.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/SUPGUYS/main/Protected%20(43).lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Patrickyoutubes/impatrick/main/text.lua"))()
